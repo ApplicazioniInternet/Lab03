1b7d4b3efdf752c94b0aa7ed303353d3b15805b6
EugenioGallea <gallea6@gmail.com>
2018-05-06 22:49:50 +0200

    Commit iniziale

--- src/main/java/it/polito/ai/lab03/repository/Position.java
+++ src/main/java/it/polito/ai/lab03/repository/Position.java
@@ -1,4 +1,40 @@
 package it.polito.ai.lab03.repository;
 
 public class Position {
+    private double latitude;
+    private double longitude;
+    private long timestamp;
+
+    public Position() {
+    }
+
+    public Position(double latitude, double longitude, long timestamp) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.timestamp = timestamp;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
