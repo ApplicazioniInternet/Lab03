f9b8badd8fab5aa91f6a019e0036d6196d00686e
EugenioGallea <gallea6@gmail.com>
2018-05-06 22:59:01 +0200

    Commit iniziale

--- src/main/java/it/polito/ai/lab03/controller/PositionController.java
+++ src/main/java/it/polito/ai/lab03/controller/PositionController.java
@@ -3,6 +3,7 @@ package it.polito.ai.lab03.controller;
 import it.polito.ai.lab03.repository.Position;
 import it.polito.ai.lab03.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -27,10 +28,39 @@ public class PositionController {
         this.positionService = ps;
     }
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    // Restituisce una collezione di positions
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     @ResponseStatus(value = HttpStatus.OK)
     public @ResponseBody
     List<Position> getAll(){ // Bisognerebbe creare la classe Position che risiede nel data layer.
         return positionService.getAll();
+    }
+
+    // Aggiungiamo una posizione a quelle che già abbiamo
+    @RequestMapping(
+            method = RequestMethod.POST
+    )
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void addPosition(@Param(value = "position") Position position) {
+    }
+
+    // Credo sia così che funzioni la questione dei path. Questo si aggiunge a quello della classe
+    @RequestMapping(
+            path = "/{id}",
+            method = RequestMethod.GET
+    )
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void getPosition(@PathVariable(value = "id") long positionId) {
+    }
+
+    @RequestMapping(
+            path = "/{id}",
+            method = RequestMethod.PUT
+    )
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void updatePosition(@PathVariable(value = "id") long positionId) {
     }
 }
