8cdaea46748458f1b8280f29fe551fb38da5070d
EugenioGallea <gallea6@gmail.com>
2018-05-09 13:47:24 +0200

    Minor fix

--- src/main/java/it/polito/ai/lab03/configuration/Oauth2Configuration.java
+++ src/main/java/it/polito/ai/lab03/configuration/Oauth2Configuration.java
@@ -2,11 +2,8 @@ package it.polito.ai.lab03.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -23,8 +20,11 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @EnableAuthorizationServer
 public class Oauth2Configuration extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
     private UserDetailsService userDetailsService;
+
+    public Oauth2Configuration(@Qualifier(value = "userDetailsImpl") @Autowired UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
